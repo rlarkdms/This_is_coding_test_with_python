24d5cbe663de4366462a12470adb0004ac146df0
repl.it user <replituser@example.com>
2021-01-24 14:56:20 +0000

    again

--- part_2 Chapter03 Implementation/4-2.py
+++ part_2 Chapter03 Implementation/4-2.py
@@ -1,3 +1,12 @@
-time=int(input())
-#이문제는 약간의 노가다성 문제네 ㅎㅎ
+t=int(input())
 
+
+count=0
+
+for i in range(0,t+1):
+  for j in range(0,60):
+    for k in range(0,60):
+      if str(str(i)+str(j)+str(k)).find("3")!=-1:
+        count+=1
+
+print(count)
---
+++ part_2 Chapter03 Implementation/4-3.py
@@ -0,0 +1,12 @@
+aa=input()
+
+# this exam should know number of cases
+# example right 2, up 1 / right 2 down 1 
+# left 2 up 1 /left 2 down 1
+# up 2 right 1/ up 2 left 1
+# down 2 right 1 /down 2 left 1
+a1=ord(aa[0])-64
+b1=int(aa[1])
+
+array=[[2,1],[2,-1],[-2,1],[-2,1],[1,2],[1,-2],[-1,2],[-1,-2]]
+
